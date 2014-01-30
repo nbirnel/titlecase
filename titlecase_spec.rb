57b6b5781b939bd0858a1778a38b0974fcd68f16
Noah Birnel <nbirnel@gmail.com>
2014-01-30 12:06:01 -0800

    make some tests

--- titlecase_spec.rb
+++ titlecase_spec.rb
@@ -6,17 +6,35 @@ describe "tc_word" do
   it "titlecases a single word" do
     tc_word("foo").should eq "Foo"
   end
+  
+  #I think I have to write my own splitter. Or have tc_word do the wrapped 
+  #punctuation handling?
+  it "Ignores external apostrophes" do
+    pending
+    tc_line("'thing'").should eq "'Thing'"
+  end
+
+  it "sees internal apostrophes" do
+    tc_line("don't").should eq "Don't"
+  end
+
+  it "ignores camelcase" do
+    tc_line("iPhone"). should eq "iPhone"
+  end
+
+  it "ignores short preposition, conjunction, and articles" do
+    tc_line("to"). should eq "to"
+  end
+
 end
 
-
-describe "#split" do
-  it "splits strings to words" do
-    split(string).should eq ["don't", "stop", "thinking", "about", "AT&T", "'t", "Horne", "McDonald", "iPhone", "globular"]
+describe "tc_line" do
+  it "basically works" do
+    tc_line("do it").should eq "Do It"
   end
-end
 
-describe "#tc_phrase" do
-  it "titlecases two words" do
-    tc_phrase("foo bar").should eq ["Foo Bar"]
+  it "retains punctuation" do
+    tc_line("shazam! what? don't go, young man...").should eq "Shazam! What? Don't Go, Young Man..."
   end
+
 end
