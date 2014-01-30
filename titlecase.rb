42c54eb669df08d4463bc15e90730ac42aaa83e6
Noah Birnel <nbirnel@gmail.com>
2014-01-30 15:07:45 -0800

    refactor ifthenelse -> ternary, rename variables

--- titlecase.rb
+++ titlecase.rb
@@ -1,28 +1,21 @@
 #!/usr/bin/env ruby
 
 def tc_word word, index = 1
+  word.sub(/^[^[:alpha:]]*[[:alpha:]]/){|first| first.upcase}
+end
+
+def tc_line line
   short_articles = ["a", "an", "the"]
   short_prepositions = ["on", "in", "to", "by", "for", "at", "of", "as", "off", "as", "out", "up" ]
   short_conjunctions = [ "and", "but", "nor", "or", "yet", "so" ]
   # write my own splitter to get rid of this punctuation junk? No... do as regex?
   abbrevs = ["re", "re:", "w/", "w", "etc.", "etc"]
-  ignore_these = short_articles + short_prepositions + short_conjunctions + abbrevs
+  ignore = short_articles + short_prepositions + short_conjunctions + abbrevs
 
-  case word
-  when /[[:upper:]]/
-    word
-  when { index == 0 }
-    word.sub(/^[^[:alpha:]]*[[:alpha:]]/){|first| first.upcase}
-  when *ignore_these
-    word
-  else
-    word.sub(/^[^[:alpha:]]*[[:alpha:]]/){|first| first.upcase}
-  end
-end
-
-def tc_line line
   tc_words =  line.split.map.with_index do |word, index|
-    tc_word(word, index) 
+    (word =~ /[[:upper:]]/) || ( index > 0 && ignore.include?(word)) \
+      ? word \
+      : tc_word(word)
   end
   tc_words.join(' ')
 end
