8dadfb8d863658425f62c5d0f07020c261b08e34
Noah Birnel <nbirnel@gmail.com>
2014-01-30 12:07:22 -0800

    move titlecase to titlecase.rb. Hopefully temporary

---
+++ titlecase.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+def tc_word word
+  short_articles = ["a", "an", "the"]
+  short_prepositions = ["on", "in", "to", "by", "for", "at", "of", "as", "off", "as", "out", "up" ]
+  short_conjunctions = [ "and", "but", "nor", "or", "yet", "so" ]
+  # write my own splitter to get rid of this punctuation junk? No... do as regex?
+  abbrevs = ["re", "re:", "w/", "w", "etc.", "etc"]
+  ignore = short_articles + short_prepositions + short_conjunctions + abbrevs
+
+  case word
+  when /[[:upper:]]/, *ignore
+    word
+  else
+    word.sub(/^./){|first| first.upcase}
+  end
+end
+
+def tc_line line
+  tc_words =  line.split.map do |word|
+    tc_word(word) 
+  end
+  tc_words.join(' ')
+end
+
+loop { case ARGV[0]
+  when /--/ then ARGV.shift; break
+  when /^-/ then usage("Unknown option: #{ARGV[0].inspect}")
+  else break
+end; }
+
+while gets() do
+  line = $_.chomp
+  tced = tc_line line
+  puts tced
+end   
