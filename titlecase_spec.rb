938db8bfbd6041ccd9178faa78a297a1cd6fb22e
Noah Birnel <nbirnel@gmail.com>
2014-02-14 13:43:21 -0800

    rename methods and clean

--- titlecase_spec.rb
+++ titlecase_spec.rb
@@ -3,56 +3,56 @@ load  "#{File.dirname(__FILE__)}/titlecase"
 #FIXME can't seem to do the fancy ruby native relative path thing
 # without naming our program titlecase.rb, which I don't want
 
-describe "tc_word" do
+describe "titlecase_word" do
   it "titlecases a single word" do
-    tc_word("foo").should eq "Foo"
+    titlecase_word("foo").should eq "Foo"
   end
   
   it "Ignores external apostrophes" do
-    tc_word("'thing'").should eq "'Thing'"
+    titlecase_word("'thing'").should eq "'Thing'"
   end
 
   it "sees internal apostrophes" do
-    tc_word("don't").should eq "Don't"
+    titlecase_word("don't").should eq "Don't"
   end
 
 end
 
-describe "tc_line" do
+describe "titlecase_line" do
   it "basically works" do
-    tc_line("do it").should eq "Do It"
+    titlecase_line("do it").should eq "Do It"
   end
 
   it "ignores short preposition, conjunction, and articles in middle " do
-    tc_line("go to").should eq "Go to"
+    titlecase_line("go to").should eq "Go to"
   end
 
   it "ignores 'words' with mixed numbers" do
-    tc_line("buy me a x0xb0x").should eq "Buy Me a x0xb0x"
+    titlecase_line("buy me a x0xb0x").should eq "Buy Me a x0xb0x"
   end
 
   it "capitalizes the first word even it it's on the ignore list" do
-    tc_line("to France").should eq "To France"
+    titlecase_line("to France").should eq "To France"
   end
 
   it "but doesn't capitalize the first word if it's camelcased" do
-    tc_line("eTrade is still around").should eq "eTrade Is Still Around"
+    titlecase_line("eTrade is still around").should eq "eTrade Is Still Around"
   end
 
   it "Ignores external apostrophes in exceptions" do
-    tc_line("don't go 'to' Chicago").should eq "Don't Go 'to' Chicago"
+    titlecase_line("don't go 'to' Chicago").should eq "Don't Go 'to' Chicago"
   end
 
   it "retains punctuation" do
-    tc_line("shazam! what? don't go, young man...").should eq "Shazam! What? Don't Go, Young Man..."
+    titlecase_line("shazam! what? don't go, young man...").should eq "Shazam! What? Don't Go, Young Man..."
   end
 
   it "ignores mixed case even as first word" do
-    tc_line("iPhone").should eq "iPhone"
+    titlecase_line("iPhone").should eq "iPhone"
   end
 
   it "doesn't falsely capitalize Dutch abbreviations" do
     pending
-    tc_line("Mr 't Horne went to 's Grave").should eq "Mr 't Horne went to 's Grave"
+    titlecase_line("Mr 't Horne went to 's Grave").should eq "Mr 't Horne went to 's Grave"
   end
 end
