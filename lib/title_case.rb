8626aa2ba95b88d76f81eff90f0922bb3fa5da6f
Noah Birnel <nbirnel@gmail.com>
2014-02-19 10:02:44 -0800

    make a string method and rename all in snake case

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,36 +1,27 @@
-def titlecase_word word
-  # the non-alpha beginning lets us capitalize "'the'" to "'The'",
-  word.sub(/^[^[:alpha:]]*[[:alpha:]]/){|first| first.upcase}
-end
+class String
 
-def titlecase_line line
-  art  = "(a)|(an)|(the)"
-  prep = "(on)|(in)|(to)|(by)|(for)|(at)|(of)|(as)|(off)|(as)|(out)|(up)"
-  part = "(if)"
-  conj = "(and)|(but)|(nor)|(or)|(yet)|(so)"
-  abbr = "(re)|(w)|(etc)|(v)|(vs)"
-  ignore_raw = '(' + [art, prep, part, conj, abbr].join('|') + ')'
-  punct_wrap = "[^[:alpha:]]*"
-  ignore = Regexp.new('^' + punct_wrap + ignore_raw + punct_wrap + '$')
-
-  titlecase_words =  line.split.map.with_index do |word, index|
-    (word =~ /[[:upper:][:digit:]]/) || ( index > 0 && ignore.match(word)) \
-      ? word \
-      : titlecase_word(word)
+  def title_case_word
+    # the non-alpha beginning lets us capitalize "'the'" to "'The'",
+    self.sub(/^[^[:alpha:]]*[[:alpha:]]/){|first| first.upcase}
   end
 
-  titlecase_words.join(' ')
+  def title_case_line
+    art  = "(a)|(an)|(the)"
+    prep = "(on)|(in)|(to)|(by)|(for)|(at)|(of)|(as)|(off)|(as)|(out)|(up)"
+    part = "(if)"
+    conj = "(and)|(but)|(nor)|(or)|(yet)|(so)"
+    abbr = "(re)|(w)|(etc)|(v)|(vs)"
+    ignore_raw = '(' + [art, prep, part, conj, abbr].join('|') + ')'
+    punct_wrap = "[^[:alpha:]]*"
+    ignore = Regexp.new('^' + punct_wrap + ignore_raw + punct_wrap + '$')
+
+    title_case_words =  self.split.map.with_index do |word, index|
+      (word =~ /[[:upper:][:digit:]]/) || ( index > 0 && ignore.match(word)) \
+        ? word \
+        : word.title_case_word
+    end
+
+    title_case_words.join(' ')
+  end
+
 end
-
-# Boilerplate for adding flags.
-loop { case ARGV[0]
-  when /--/ then ARGV.shift; break
-  when /^-/ then usage("Unknown option: #{ARGV[0].inspect}")
-  else break
-end; }
-
-while gets() do
-  line = $_.chomp
-  tced = titlecase_line line
-  puts tced
-end   
